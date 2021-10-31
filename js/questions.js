89916a41ad3107c9c62ed62d683dc2fa0d26001e
ashima223 <85673383+ashima223@users.noreply.github.com>
2021-10-31 12:28:23 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "HTML is considered as__language",
+    answer: "Markup Language",
+    options: [
+      "Programming Language",
+      "OOP Language",
+      "High Level Language",
+      "Markup Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "HTML tags are surrounded by__brackets",
+    answer: "Angle",
+    options: [
+      "Angle",
+      "Square",
+      "Round",
+      "Curly"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The property in CSS used to change the background color of an element is -",
+    answer: "background-color",
+    options: [
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following CSS property is used to set the background image of an element?",
+    answer: "background-image",
+    options: [
+      "background-attachment",
+      "background-image",
+      "background-color",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 5,
+    question: "JavaScript language is ___",
+    answer: "Object-Based Language",
+    options: [
+      "Object-Oriented Language",
+      "Object-Based Language",
+      "Assembly-language",
+      "High-level Language"
+    ]
+  },
+];
